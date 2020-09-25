ee9744796a086231e364190a94f2310963aad10f
Mwiza Simbeye <mwiza@agripredict.com>
2020-09-25 16:44:51 +0200

    Created basic homepage for ReactJS blogpage

---
+++ src/pages/homepage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomePage = () => (
+    <React.Fragment>
+        <h1>Hello, Welcome to my Blog</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer metus eros, consequat eu maximus nec, euismod vitae lacus. Cras et tellus et magna venenatis molestie. Ut pretium orci leo, condimentum hendrerit orci iaculis et. Proin ac sollicitudin orci, sit amet blandit velit. Mauris at orci et eros suscipit semper. Sed a iaculis tortor. Nam vitae ipsum blandit, pretium velit ac, lacinia nibh. Aliquam magna leo, imperdiet quis auctor ut, volutpat sit amet elit. Integer eu quam mauris. Pellentesque blandit magna in tellus ultricies, vel tristique nisi hendrerit. Vestibulum vehicula mollis sem, at vulputate felis finibus quis. Vestibulum egestas accumsan nulla non commodo. Curabitur vel ipsum neque. Vivamus accumsan eleifend diam.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer metus eros, consequat eu maximus nec, euismod vitae lacus. Cras et tellus et magna venenatis molestie. Ut pretium orci leo, condimentum hendrerit orci iaculis et. Proin ac sollicitudin orci, sit amet blandit velit. Mauris at orci et eros suscipit semper. Sed a iaculis tortor. Nam vitae ipsum blandit, pretium velit ac, lacinia nibh. Aliquam magna leo, imperdiet quis auctor ut, volutpat sit amet elit. Integer eu quam mauris. Pellentesque blandit magna in tellus ultricies, vel tristique nisi hendrerit. Vestibulum vehicula mollis sem, at vulputate felis finibus quis. Vestibulum egestas accumsan nulla non commodo. Curabitur vel ipsum neque. Vivamus accumsan eleifend diam.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer metus eros, consequat eu maximus nec, euismod vitae lacus. Cras et tellus et magna venenatis molestie. Ut pretium orci leo, condimentum hendrerit orci iaculis et. Proin ac sollicitudin orci, sit amet blandit velit. Mauris at orci et eros suscipit semper. Sed a iaculis tortor. Nam vitae ipsum blandit, pretium velit ac, lacinia nibh. Aliquam magna leo, imperdiet quis auctor ut, volutpat sit amet elit. Integer eu quam mauris. Pellentesque blandit magna in tellus ultricies, vel tristique nisi hendrerit. Vestibulum vehicula mollis sem, at vulputate felis finibus quis. Vestibulum egestas accumsan nulla non commodo. Curabitur vel ipsum neque. Vivamus accumsan eleifend diam.
+        </p>
+    </React.Fragment>
+)
+
+export default HomePage;
