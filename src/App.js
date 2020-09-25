d4978f99e06622bc1ece7897a81cac27c9e980d8
Mwiza Simbeye <mwiza@agripredict.com>
2020-09-25 16:58:14 +0200

    added routing for new pages (about, articles and article-list)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 
 import React, { Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom'
 
+import AboutPage from './pages/AboutPage'
+import Article from './pages/AboutPage'
+import ArticleList from './pages/ArticleList'
 import HomePage from './pages/homepage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HomePage/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" component={HomePage} exact/>
+          <Route path="/about" component={AboutPage}/>
+          <Route path="/articles-list" component={ArticleList}/>
+          <Route path="/article" component={Article}/>
+        </div>
+      </Router>
     );
   }
 }
