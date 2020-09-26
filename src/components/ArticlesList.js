3988b0a37b36585c8bb7bf6801371482803b6d5a
Mwiza Simbeye <mwiza@agripredict.com>
2020-09-26 10:52:02 +0200

    added new pages :)

---
+++ src/components/ArticlesList.js
@@ -0,0 +1,15 @@
+import {Link} from 'react-router-dom'
+import React from 'react'
+
+const ArticlesList = ({articles}) => (
+    <>
+    {articles.map(article => (
+        <Link className="article-list-item" key={article.key} to={`/article/${article.name}`}>
+            <h3>{article.title}</h3>
+            <p>{article.content[0].substring(0, 150)}...</p>
+        </Link>
+    ))}
+    </>
+);
+
+export default ArticlesList;
